8606d560a4432d7542c7a66a206214e31c086df3
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-09-05 16:40:21 +0500

    upadted changes

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "netlify-deployments": {
+        enabled: true,
+        config: {
+            accessToken: "<netlify-access-token>",
+            sites: [
+                {
+                    name: 'Site 1',
+                    id: "696e1071-b0f3-42c6-b04b-44d987e7fe7e",
+                    buildHook: "https://syedhassanraza.netlify.app/build_hooks/<hook_id>",
+                    branch: 'develop' // optional
+                }
+            ]
+        },
+    },
+});
