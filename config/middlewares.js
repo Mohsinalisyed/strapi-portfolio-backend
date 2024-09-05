527168f85dfd74e465df742760d42cfacb0c6b1c
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-09-05 17:10:39 +0500

    update build

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,15 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173'],  // Add the origin you want to allow
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
